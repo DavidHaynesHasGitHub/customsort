f4362abe471f943fa5141940460c08ce4701eaae
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-15 15:37:22 -0800

    completed sort file. copied code from compass for adv sort

---
+++ advsort.js
@@ -0,0 +1,20 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+//this function will take the array of students and will put them in
+//alphabetical order by name or if names are matching will sort by age
+
+
+
+
+
+
+
+// var result = input.map(function(obj){
+//   return Math.sqrt(Math.pow(obj.a, 2) + Math.pow(obj.b, 2))}
+// );
+//
